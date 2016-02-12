66bafd97dd02ee2f61a279f810d39b876f1575df
Ben Carpenter <carpenter@earthlinginteractive.com>
2016-02-12 14:29:37 -0600

    flatten chain in worker

--- sender_promise.js
+++ sender_promise.js
@@ -1,35 +1,25 @@
 #!/usr/bin/env node
 
 var amqp = require('amqplib');
-var ch, conn, q;
+var ch, conn;
+
+var q = 'task_queue';
+var msg = process.argv.slice(2).join(' ') || JSON.stringify({ text:"Hello World!", version:"12345678" });
 
 amqp.connect('amqp://localhost').then(function(_conn) {
     conn = _conn;
     return conn.createChannel();
 })
 .then(function(_ch) {
-    q = 'hello';
     ch = _ch;
 
     return ch.assertQueue(q, {durable: true});
 })
 .then(function(){
-    return ch.sendToQueue(q, new Buffer('Hello World!'));
+    return ch.sendToQueue(q, new Buffer(msg));
 })
 .then(function(){
-    console.log(" [x] Sent 'Hello World!'")
+    console.log(" [x] Sent  '%s'", msg)
 });
 
 setTimeout(function() { conn.close(); process.exit(0) }, 500);
-
-//amqp.connect('amqp://localhost', function(err, conn) {
-//    conn.createChannel(function(err, ch) {
-//        var q = 'task_queue';
-//        var msg = process.argv.slice(2).join(' ') || JSON.stringify({ text:"Hello World!", version:"12345678" });
-//
-//        ch.assertQueue(q, {durable: true});
-//        ch.sendToQueue(q, new Buffer(msg), {persistent: true});
-//        console.log(" [x] Sent '%s'", msg);
-//    });
-//    setTimeout(function() { conn.close(); process.exit(0) }, 500);
-//});
--- worker_promise.js
+++ worker_promise.js
@@ -1,36 +1,21 @@
 #!/usr/bin/env node
 
 var amqp = require('amqplib');
-var ch, conn, q;
+var ch, conn;
+var q = 'task_queue';
 
 amqp.connect('amqp://localhost').then(function(_conn) {
     conn = _conn;
     return conn.createChannel();
 })
 .then(function(_ch) {
-    q = 'hello';
     ch = _ch;
-
     return ch.assertQueue(q, {durable: true});
 })
+.then(function(){ return ch.prefetch(1) })
 .then(function(){
     console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
     ch.consume(q, function(msg) {
         console.log(" [x] Received %s", msg.content.toString());
-    }, {noAck: true});
+    }, {noAck: false});
 });
-
-//amqp.connect('amqp://localhost', function(err, conn) {
-//    conn.createChannel(function(err, ch) {
-//        var q = 'task_queue';
-//
-//        ch.assertQueue(q, {durable: true});
-//        ch.prefetch(1);
-//        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
-//        ch.consume(q, function(msg) {
-//            console.log(" [x] Received :");
-//            console.dir(JSON.parse(msg.content));
-//            ch.ack(msg);
-//        }, {noAck: false});
-//    });
-//});
